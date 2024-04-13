a07e70a558f7319f5b772c3ee21cac807b9932d7
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-13 14:46:28 +0200

    connection imgDocker🐋  with pgSQL 🐘

---
+++ back_end/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'hello!'
+
+#-------------------------------------------------
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=environ.get('PORT'),debug=environ.get('DEBUG'))
